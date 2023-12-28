c0ec38392a0e77d9c76292658391799afb7e98cc
Meet19aug <meetpatel19aug@gmail.com>
2023-12-28 11:19:15 -0500

    Step 9: Component Configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,38 +1,25 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller,MessageToast, JSONModel, ResourceModel) {
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-        onInit : function () {
-            //set the data model on view
-            var oData = {
-                recipient: {
-                    name : "UI5"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel)
+      onShowHello: function () {
+        // read message from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-            // set i18n model on view
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportLocales:[""],
-                fallbackLocale:""
-            })
-            this.getView().setModel(i18nModel, "i18n")
-        },
-        onShowHello: function () {
-            // read message from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            // show a native or vanila js alert.
-            MessageToast.show(sMsg)
-        }
+        // show a native or vanila js alert.
+        MessageToast.show(sMsg);
+      },
     });
-});
+  }
+);
